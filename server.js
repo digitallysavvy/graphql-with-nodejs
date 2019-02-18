0e2c6a8281f53513a7db730a22182331e5756a1a
digitallysavvy <hermes.frangoudis@gmail.com>
2019-02-18 15:26:33 -0500

    updated express endpoint to GraphQL type

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+// required libraries
 const express = require('express');
+const graphHTTP = require('express-graphql');
+const {GraphQLSchema} = require('graphql');
+
+const {queryType} = require('./query.js');
+
+// define port and express app object
 const port = 5000;
 const app = express();
 
-app.get('/hello', (req,res) => {
-  res.send("hello");
-});
+// define the Schema
+const schema =  new GraphQLSchema({ query: queryType});
+
+// setup nodejs GraphQL server
+app.use('/graphql', graphHTTP({
+  schema: schema,
+  graphiql: true,
+}));
 
 app.listen(port);
-console.log(`Server Running at localhost:${port}`);
+console.log(`GraphQL Server Running at localhost: ${port}`);
